c4770772e98f2a69279cb9d250b351a7c67dca3c
ebfidKate <ebfid.kate@gmail.com>
2024-04-11 23:39:34 +0100

    initial react setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>eternal.food</h1>
+      <p>Наш сайт о вкуснейшей еде скоро будет доступен!</p>
+      <p className="soon">Оставайтесь с нами, скоро открытие!</p>
+    </div>
+  )
+}
+
+export default App
